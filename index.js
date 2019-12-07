a51f20e0238bb7e177da23b1e75ef273097280f7
Aleksander Wiażewicz <wiazewicz@gmail.com>
2019-12-07 19:15:59 +0100

    counter v2

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-var seconds = 12;
+function sec2time(timeInSeconds) {
+    var pad = function(num, size) { return ('000' + num).slice(size * -1); },
+    time = parseFloat(timeInSeconds).toFixed(3),
+    minutes = Math.floor(time / 60) % 60,
+    seconds = Math.floor(time - minutes * 60);
+ 
+    return pad(minutes, 2) + ':' + pad(seconds, 2);
+}
+
+var seconds = 1201;
 setInterval(function() {
     --seconds;
-    document.getElementById("countdown").textContent = seconds + ' s';
+    document.getElementById("countdown").textContent = sec2time(seconds);
     if (seconds <= 0) {
-        seconds = 12;
+        seconds = 1201;
     };
 }, 1000);
+
+
+
+
