0df060441985d7963c6d8bb4bac2c81f0110c828
Aleksander Wiażewicz <wiazewicz@gmail.com>
2019-12-11 12:21:50 +0100

    basic quiz

--- JS/quiz.js
+++ JS/quiz.js
@@ -1,6 +1,24 @@
-const question = document.getElementById('question');
+let question = document.getElementById('question');
 const answerA = document.getElementById('answer-a');
 const answerB = document.getElementById('answer-b');
 const answerC = document.getElementById('answer-c');
 const answerD = document.getElementById('answer-d');
 const submitAnswers = document.getElementById('submit-answers');
+
+const questionsArray = [
+    ['Jak masz na imię?', 'Michał', 'Olek', 'Kuba', 'Łukasz'], 
+    ['Jak się dziś masz?', 'Super', 'Spoko', 'Nijak', 'Chujowo'], 
+    ['Ile masz lat?', 'za dużo', 'Sto lat, sto lat, ...!', '21', 'za mało'], 
+    ['Jak długo śpisz?', '12 minut', '10 godzin', '7 godzin'],
+    ['Twój wynik to:', '', '', '', '']
+];
+let questionNum = -1;
+
+function nextQuestion(){
+    ++questionNum;
+    question.innerHTML = questionsArray[questionNum][0];
+    answerA.innerHTML = questionsArray[questionNum][1];
+    answerB.innerHTML = questionsArray[questionNum][2];
+    answerC.innerHTML = questionsArray[questionNum][3];
+    answerD.innerHTML = questionsArray[questionNum][4];
+}
